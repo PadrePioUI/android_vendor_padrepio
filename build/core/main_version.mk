d332ba9b38d89450df6a6ccd49b8a7acc45669ed
LeddaZ <leonardoledda@gmail.com>
2022-07-30 01:36:58 +0200

    padrepio: Keep Lineage props

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -6,20 +6,20 @@ endif
 
 # PadrePioUI System Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.padrepio.version=$(LINEAGE_VERSION) \
-    ro.padrepio.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.padrepio.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.lineageos.version=$(LINEAGE_VERSION) \
+    ro.lineageos.releasetype=$(LINEAGE_BUILDTYPE) \
+    ro.lineageos.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.modversion=$(LINEAGE_VERSION) \
-    ro.padrepiolegal.url=https://lineageos.org/legal
+    ro.lineageoslegal.url=https://lineageos.org/legal
 
 # PadrePioUI Platform Display Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.padrepio.display.version=$(LINEAGE_DISPLAY_VERSION)
+    ro.lineageos.display.version=$(LINEAGE_DISPLAY_VERSION)
 
 # PadrePioUI Platform SDK Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.padrepio.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
+    ro.lineageos.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
 
 # PadrePioUI Platform Internal Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.padrepio.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
+    ro.lineageos.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
