8056012ef67cf90179e035ded246e9814c948429
Leonardo Ledda <leonardoledda@gmail.com>
2023-08-15 12:55:07 +0200

    padrepio: Rebrand to Padre Pio UI
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -1,7 +1,7 @@
-include vendor/lineage/config/BoardConfigKernel.mk
+include vendor/padrepio/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/lineage/config/BoardConfigQcom.mk
+include vendor/padrepio/config/BoardConfigQcom.mk
 endif
 
-include vendor/lineage/config/BoardConfigSoong.mk
+include vendor/padrepio/config/BoardConfigSoong.mk
--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,11 +1,11 @@
 # Inherit common mobile Lineage stuff
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/padrepio/config/common.mk)
 
 # Include AOSP audio files
-include vendor/lineage/config/aosp_audio.mk
+include vendor/padrepio/config/aosp_audio.mk
 
 # Include Lineage audio files
-include vendor/lineage/config/lineage_audio.mk
+include vendor/padrepio/config/lineage_audio.mk
 
 # Default notification/alarm sounds
 PRODUCT_PRODUCT_PROPERTIES += \
